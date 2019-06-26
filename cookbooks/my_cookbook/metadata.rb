86fff1af10a3cbfd7e53c52babc6c859c425fd50
Jamson <qiujx@cn.ibm.com>
2019-06-26 14:54:35 +0800

    Build essential cookbook

--- cookbooks/my_cookbook/metadata.rb
+++ cookbooks/my_cookbook/metadata.rb
@@ -6,9 +6,9 @@ description 'Installs/Configures my_cookbook'
 long_description 'Installs/Configures my_cookbook'
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
-
+depends 'build-essential', '>=7.0.3'
 # The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# tracked.  A `View Issues` link wißll be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
 # issues_url 'https://github.com/<insert_org_here>/my_cookbook/issues'
