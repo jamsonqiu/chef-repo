86a499e99d8d79676bcd50f5f78a17bd91a01a73
Jamson <qiujx@cn.ibm.com>
2019-06-26 14:14:16 +0800

    commit before installing iptables

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,5 +1,10 @@
 # See http://docs.chef.io/config_rb_knife.html for more information on knife configuration options
 
+cookbook_copyright "JS"
+cookbook_license "apachev2"
+cookbook_email "qiujx@cn.ibm.com"
+
+
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
